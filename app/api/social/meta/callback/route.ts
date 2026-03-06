bfb261846c55f61db901d1279031f5151bce344e
mydfacylita <mydengenhariadesoftware@gmail.com>
2026-03-06 17:51:54 -0300

    fix: add force-dynamic to social/meta/callback to fix build

--- app/api/social/meta/callback/route.ts
+++ app/api/social/meta/callback/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic'
+
 /**
  * Callback do OAuth do Facebook/Instagram
  * GET /api/social/meta/callback
